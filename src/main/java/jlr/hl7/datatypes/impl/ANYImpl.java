85edff7510b9e6163aa437c5c2900d2d86fa14b3
unknown <jreyes@.intern.csam.no>
2010-12-22 20:13:15 +0800

    added

---
+++ src/main/java/jlr/hl7/datatypes/impl/ANYImpl.java
@@ -0,0 +1,6 @@
+package jlr.hl7.datatypes.impl;
+
+import jlr.hl7.datatypes.ANY;
+
+public class ANYImpl implements ANY {
+}
---
+++ src/main/java/jlr/hl7/rim/impl/EntityImpl.java
@@ -0,0 +1,6 @@
+package jlr.hl7.rim.impl;
+
+import jlr.hl7.rim.Entity;
+
+public class EntityImpl implements Entity {
+}
